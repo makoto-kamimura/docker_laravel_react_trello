9b61f90014401dceefa3455c2ca52257195a8b85
Makoto Kamimura <68632305+makoto-kamimura@users.noreply.github.com>
2025-03-02 12:11:08 +0900

    update docker

--- docker/app/entrypoint.sh
+++ docker/app/entrypoint.sh
@@ -3,21 +3,14 @@ set -e
 
 cd /var/www/html 
 
-echo "🚀 Laravel コンテナ起動中..."
-
 # .env ファイルが存在しない場合、デフォルトをコピー
 if [ ! -f /var/www/html/.env ]; then
-    echo "📄 .env ファイルが見つからないため、.env.example からコピーします..."
     cp /var/www/html/.env.example /var/www/html/.env
-else
-    echo "✅ .env ファイルがすでに存在します"
 fi
 
-echo "🔑 Laravel アプリケーションキーを生成..."
-php artisan key:generate || echo "⚠️ key:generate に失敗しました"
+php artisan key:generate || echo "⚠️ key:generate error"
 
-echo "🛠 マイグレーションを実行..."
-php artisan migrate --force --seed  || echo "⚠️ マイグレーションに失敗しました"
+php artisan migrate --force --seed  || echo "⚠️ migration error"
 
 npm install
 
@@ -25,12 +18,10 @@ npm install
 npx update-browserslist-db@latest
 
 # MUIとアイコンライブラリをインストール
-echo "📦 MUIとアイコンライブラリをインストール..."
-npm install @mui/icons-material @mui/material @emotion/react @emotion/styled || echo "⚠️ MUIのインストールに失敗しました"
+npm install @mui/icons-material @mui/material @emotion/react @emotion/styled || echo "⚠️ MUI install error"
 
 # ビルドを実行
 # npm run hot
 npm run production --no-cache
 
-echo "🚀 Apache を起動..."
 exec apache2-foreground
